166618618df35fbce677170eb78ae3e2b4685f0f
Chris <chrisdel@protonmail.com>
2020-08-04 20:00:34 -0400

    first solution not scaleable

--- matrix_spiral_copy.py
+++ matrix_spiral_copy.py
@@ -2,21 +2,38 @@ def spiral_copy(inputMatrix):
   copy_array = []
   first_array = inputMatrix[0]
   number_of_columns = len(inputMatrix[0])
+  number_of_rows = len(inputMatrix)
+  bottom_array = inputMatrix[number_of_rows - 1]
+  
   columns = 0
-  y = 1
-  print("number of columns", number_of_columns)
+  y = 0
+  z = 0
+
   for x in first_array:
     copy_array.append(x)
     
-  for columns in inputMatrix[y]:
-    copy_array.append(columns[number_of_columns - 1])
+  for x in range(1, number_of_columns - 1):
+    
+    copy_array.append(inputMatrix[x][number_of_columns-1])
+  
+  for x in range(0, number_of_columns - 1):
+    copy_array.append(bottom_array[(number_of_columns - 2) - y])
     y += 1
     
-  return copy_array
+  for x in range(1, number_of_rows - 1):
+    copy_array.append(inputMatrix[(number_of_rows - 2) - z][0])
+    z += 1
+  for x in range(1, number_of_columns - 1):
+    copy_array.append(inputMatrix[1][x])
   
+  for x in range(2, number_of_columns - 2):
+    copy_array.append(inputMatrix[x][ number_of_columns-2])
     
+  for x in range(2, number_of_columns - 1):
+    copy_array.append(inputMatrix[number_of_rows - 2][(number_of_columns - 1)- x])
+    z += 1 
   
-      
+  return copy_array      
   
       
     
